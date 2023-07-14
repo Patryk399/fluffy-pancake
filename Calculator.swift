0dae53e2be8e409dc1efb413b46b568facddd3c9
P.K Woźny <121348461+Patryk399@users.noreply.github.com>
2023-07-14 09:04:27 +0200

    Create Calculator.swift
    Begin of my story...

---
+++ Calculator.swift
@@ -0,0 +1,11 @@
+// Function to calculate the sum of two numbers
+func calculateSum(num1: Int, num2: Int) -> Int {
+    return num1 + num2
+}
+
+// Example usage
+let number1 = 5
+let number2 = 10
+
+let sum = calculateSum(num1: number1, num2: number2)
+print("The sum of \(number1) and \(number2) is \(sum).")
